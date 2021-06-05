45e084b94b5d57aae975e230b26e5d12dc6dc329
Sang-hyun Park <hyun06000@gmail.com>
2021-06-05 10:22:32 +0000

    tmp

--- models/ResBlock.py
+++ models/ResBlock.py
@@ -11,19 +11,19 @@ class ResBlock(Layer):
         
     def build(self, input_shape):
         
-        self.LN_1 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
+        #self.LN_1 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
         self.conv_1 = Conv2D(self.C, (3, 3), strides = (1, 1), padding='same', activation = None)
-        self.LN_2 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
+        #self.LN_2 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
         self.conv_2 = Conv2D(self.C, (3, 3), strides = (1, 1), padding='same', activation = None)
         
-        self.LN_3 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
+        #self.LN_3 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
         self.conv_3 = Conv2D(self.C, (3, 3), strides = (1, 1), padding='same', activation = None)
-        self.LN_4 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
+        #self.LN_4 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
         self.conv_4 = Conv2D(self.C, (3, 3), strides = (1, 1), padding='same', activation = None)
         
-        self.LN_5 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
+        #self.LN_5 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
         self.conv_5 = Conv2D(self.C, (3, 3), strides = (1, 1), padding='same', activation = None)
-        self.LN_6 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
+        #self.LN_6 = tf.keras.layers.LayerNormalization(axis = [1, 2, 3], epsilon=1e-8)
         self.conv_6 = Conv2D(self.C, (3, 3), strides = (1, 1), padding='same', activation = None)
         
         self.gelu = tf.keras.activations.gelu
@@ -38,20 +38,19 @@ class ResBlock(Layer):
             raise ValueError("Invalid pooling")
     def call(self, x):
         
-        x = tf.tile(x,[1,1,1,2])
-        _x = self.conv_1(self.gelu(self.LN_1(x)))
-        _x = self.conv_2(self.gelu(self.LN_2(_x)))
-        _x = _x + x
+        _x = self.gelu(self.conv_1(x))
+        _x = self.gelu(self.conv_2(_x))
+        #_x = _x + x
         
         x = _x
-        _x = self.conv_3(self.gelu(self.LN_3(x)))
-        _x = self.conv_4(self.gelu(self.LN_4(_x)))
-        _x = _x + x
+        _x = self.gelu(self.conv_3(x))
+        _x = self.gelu(self.conv_4(_x))
+        #_x = _x + x
         
         x = _x
-        _x = self.conv_5(self.gelu(self.LN_5(x)))
-        _x = self.conv_6(self.gelu(self.LN_6(_x)))
-        _x = _x + x
+        _x = self.gelu(self.conv_5(x))
+        _x = self.gelu(self.conv_6(_x))
+        #_x = _x + x
         
         x = self.pool(_x)
         
