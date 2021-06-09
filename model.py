3f6fb3baa78f7e0fc2bafbec11ce4d32ddca1f31
Sang-hyun Park <hyun06000@gmail.com>
2021-06-09 14:37:03 +0000

    20210619 model.py

--- model.py
+++ model.py
@@ -3,169 +3,79 @@
 
 # In[1]:
 
-
 import os
-import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import datetime
 import tensorflow_datasets as tfds
 
+from models.ResBlock import resnet
+from modules.TFR_load import TFR_load
 
-from preprocessing.data.imagenet_labels import imagenet_labels
 
-from modules.modified_ViT_Layers import PatchEmbedding, ClassToken, Epos, ViT_Block
 
-def decode_fn(example):
-    image = example["image"]
-    image = tf.cast(image, tf.float32)
-    label = example["label"]
-    label = tf.one_hot(label,10)
-    return image, label
+# TODO
+# config.py
 
-batch_size = 256
+# Hyper params
+
+BATCH_SIZE     = 256
+NUM_OF_CLASS   = 10
+LEARNING_RATE  = 1e-5
+COSINE_DECAY   = True
+NUM_TRAIN_DATA = 60000
+NUM_TEST_DATA  = 10000
+EPOCH = 100
+
+TR_STEPS_PER_EPOCH = NUM_TRAIN_DATA//BATCH_SIZE
+TE_STEPS_PER_EPOCH = NUM_TEST_DATA//BATCH_SIZE
+
+
+# data loard
+
 ds_name = 'cifar10'
-tr_ds, te_ds = tfds.load(ds_name,
-                         split = ['train', 'test'],
-                         shuffle_files = True)
+builder = tfds.builder(ds_name)
 
-tr_ds = tr_ds.map(decode_fn)\
-                .repeat()\
-                .batch(batch_size)\
-                .prefetch(tf.data.AUTOTUNE)
-te_ds = te_ds.map(decode_fn)\
-                .repeat()\
-                .batch(batch_size)\
-                .prefetch(tf.data.AUTOTUNE)
+tr_ds, te_ds = builder.as_dataset(split = ['train', 'test'], shuffle_files = True)
 
+tr_ds = tr_ds.batch(BATCH_SIZE)
+te_ds = te_ds.batch(BATCH_SIZE)
 
-class MyModel(tf.keras.Model):
-    def __init__(self, batch_size):
-        super(MyModel, self).__init__()
-        
-        self.i = 1
-        self.batch_size = batch_size
-        
-        self.PatchEmbedding = PatchEmbedding(D = 128,
-                                             patch_size = 16)
-        #self.ClassToken = ClassToken()
-        #self.Epos = Epos()
-        
-        num_of_block = 16
-        
-        self.Block = []
-        for _ in range(num_of_block):
-            self.Block.append(ViT_Block(hidden_size = 128))
-        
-        self.flatten = tf.keras.layers.Flatten()
-        
-        self.dense = tf.keras.layers.Dense(
-                                units = 10, # of class
-                                activation=None,
-                                use_bias=True,
-                                kernel_regularizer = tf.keras.regularizers.l2(0.1))
+model = resnet
 
-        self.softmax = tf.keras.layers.Softmax(axis=-1)
-        #self.LN = tf.keras.layers.LayerNormalization(axis = -1,epsilon=1e-8)
-        
-    def call(self, x):
-        
-        print('EPOCH ::: ',self.i)
-        self.i += 1
-        #CIFAR-10
-        x = tf.ensure_shape(x, [self.batch_size,32,32,3])
-        
-        x = self.PatchEmbedding(x)
-        print('input ::: ', x)
-        
-        for block in self.Block:
-            x = block(x)
-        
-        x = self.flatten(x)
-        x = self.dense(x)
-        x = self.softmax(x)
-        
-        return x
-
-model = MyModel(batch_size)
-
-
-
-#decay_steps = 50000//batch_size * 100
-#initial_learning_rate = 1e-3
-#lr_decayed_fn = tf.keras.optimizers.schedules.CosineDecay(
-#    initial_learning_rate, decay_steps,alpha=0.01)
-model.compile(
-    optimizer=tf.keras.optimizers.SGD(learning_rate = 1e-4),
-    loss=tf.keras.losses.CategoricalCrossentropy(),
-    metrics=[tf.keras.metrics.CategoricalAccuracy()],
-)
-
-current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-train_log_dir = 'logs/' + current_time
-
-tb_cb = tf.keras.callbacks.TensorBoard(
-    log_dir=train_log_dir,
-    update_freq='epoch')
-
-model.fit(
-    tr_ds,
-    batch_size=batch_size,
-    epochs=50000,
-    callbacks=[tb_cb],
-    validation_data = te_ds,
-    steps_per_epoch = 50000//batch_size,
-    validation_steps = 10000//batch_size,
-    verbose = 0
-)
-
-
-# Calling `save('my_model.h5')` creates a h5 file `my_model.h5`.
-model.save("my_h5_model.h5")
-
-# It can be used to reconstruct the model identically.
-#reconstructed_model = keras.models.load_model("my_h5_model.h5")
-
-
-#batch_size=128,
-'''
-model.fit_generator(
-    generator = train_ds, steps_per_epoch=100, epochs=10, verbose=1, callbacks=[tb_cb],
-    validation_data=val_ds, validation_steps=100, validation_freq=1,
-    class_weight=None, max_queue_size=10, workers=1, use_multiprocessing=False,
-    shuffle=True, initial_epoch=0
-)
-'''
-
-
-'''
-optimizer = tf.keras.optimizers.Adam(learning_rate=1e-6)
+optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE, epsilon=1e-08)
 loss = tf.keras.losses.CategoricalCrossentropy()
-#loss = tf.keras.losses.MeanSquaredError()
-
-# In[ ]:
+accuracy = tf.keras.metrics.CategoricalAccuracy()
 
 current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 train_log_dir = 'logs/gradient_tape/' + current_time + '/train'
-train_gph_dir = 'logs/graph/' + current_time + '/'
+test_log_dir = 'logs/gradient_tape/' + current_time + '/test'
 train_summary_writer = tf.summary.create_file_writer(train_log_dir)
-#writer = tf.summary.create_file_writer(train_gph_dir)
+test_summary_writer = tf.summary.create_file_writer(test_log_dir)
 
-with tf.compat.v1.Session() as sess:
-    summary_writer = tf.compat.v2.train.SummaryWriter(train_gph_dir, sess.graph)
-
-i = 0
-for images,label in ds:
-    with tf.GradientTape() as tape:
-        logits = model(images)
-        loss_value = loss(logits, tf.one_hot(label,1000))
-
-    with train_summary_writer.as_default():
-        tf.summary.scalar('loss', loss_value, step=i)
-    
-    grads = tape.gradient(loss_value, model.trainable_variables)
-    optimizer.apply_gradients(zip(grads, model.trainable_variables))
-    
-    i += 1
-    #print(loss_value.numpy())
-'''
+step = 1
+for EP in range(EPOCH):
+    for tr_example, te_example in zip(tr_ds, te_ds):
+        images, labels = tr_example["image"], tr_example["label"]
+        with tf.GradientTape() as tape:
+            logits = model(images / 255)
+            loss_value = loss(tf.one_hot(labels, 10), logits)
+            accuracy.update_state(tf.one_hot(labels, 10), logits)
+            with train_summary_writer.as_default():
+                tf.summary.scalar('loss', loss_value, step=step)
+                tf.summary.scalar('acc', accuracy.result(), step=step)
+                accuracy.reset_state()
+        grads = tape.gradient(loss_value, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        
+        
+        images, labels = te_example["image"], te_example["label"]
+        logits = model(images / 255)
+        loss_value = loss(tf.one_hot(labels, 10), logits)
+        accuracy.update_state(tf.one_hot(labels, 10), logits)
+        with test_summary_writer.as_default():
+            tf.summary.scalar('loss', loss_value, step=step)
+            tf.summary.scalar('acc', accuracy.result(), step=step)
+            accuracy.reset_state()
+        step += 1
--- models/ResBlock.py
+++ models/ResBlock.py
@@ -7,22 +7,71 @@ from tensorflow.keras.layers import BatchNormalization, Conv2D, Activation, Dens
 
 
 
-input_tensor = Input(shape=(224, 224, 1), dtype='float32', name='input')
+input_tensor = Input(shape=(32, 32, 3), dtype='float32', name='input')
 
 def conv1_layer(x):
     x = ZeroPadding2D(padding=(3, 3))(x) # "same" padsize is 5.. 
-    x = Conv2D(64, (7, 7), stride=(2, 2))
+    x = Conv2D(64, (7, 7), strides=(2, 2))(x)
     x = BatchNormalization()(x)
-    x = Axtivation('relu')(x)
-    x = Zeropadding2D(padding = (1, 1)) # why?
+    x = Activation('relu')(x)
+    x = ZeroPadding2D(padding = (1, 1))(x) # why?
     
     return x
 
-def conv2_layer(x):
-    x = MaxPooling2D((3, 3), 2)(x)
+def ResBlock(x, C, P = True):
+    if P:
+        x = MaxPooling2D((3, 3), 2)(x)
     
     shortcut = x
     for i in range(3):
         if not i:
-            x = Conv2D(64, (1, 1), stride = (1, 1))(x)
+            x = Conv2D(C, (1, 1), strides = (1, 1))(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
             
+            x = Conv2D(C, (3, 3), strides = (1, 1), padding = 'same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+            
+            x = Conv2D(C * 4, (1, 1), strides = (1, 1))(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+            
+            shortcut = Conv2D(C * 4, (1, 1), strides = (1, 1))(shortcut)
+            shortcut = BatchNormalization()(shortcut)
+            
+            x = Add()([x,shortcut])
+            x = Activation('relu')(x)
+            
+            shortcut = x
+        
+        else:
+            x = Conv2D(C, (1, 1), strides = (1, 1))(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+            
+            x = Conv2D(C, (3, 3), strides = (1, 1), padding = 'same')(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+            
+            x = Conv2D(C * 4, (1, 1), strides = (1, 1))(x)
+            x = BatchNormalization()(x)
+            x = Activation('relu')(x)
+            
+            x = Add()([x,shortcut])
+            x = Activation('relu')(x)
+            
+            shortcut = x
+    return x
+
+x = conv1_layer(input_tensor)
+x = ResBlock(x,64, False)
+x = ResBlock(x,128,False)
+x = ResBlock(x,256,)
+x = ResBlock(x,512,)
+x = ResBlock(x,1024)
+
+x = GlobalAveragePooling2D()(x)
+output_tensor = Dense(10, activation = 'softmax')(x)
+
+resnet = Model(input_tensor,output_tensor)
